accafccaddf59a662275f6f850aee99e8e4751ff
ritwikdurga <142917963+ritwikdurga@users.noreply.github.com>
2023-12-21 19:16:24 +0530

    feat: added signin, signup Webpages. Added API Implementation for the signin, signup in Popup.

--- Popup/signin_popup.js
+++ Popup/signin_popup.js
@@ -3,3 +3,48 @@ const backButton = document.getElementById('backButton');
 backButton.addEventListener('click', () => {
     window.location.href = 'popup.html';
 });
+
+//Signin API Implementation.
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.form.login');
+    
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        
+        // Get form data
+        const formData = new FormData(form);
+        const userData = {
+            username: formData.get('username'),
+            password: formData.get('password')
+        };
+        
+        try {
+            const response = await fetch('https://your-api-endpoint.com/validate', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // Add any other necessary headers for authentication or security tokens
+                },
+                body: JSON.stringify(userData)
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+
+            // Parse the API response
+            const responseData = await response.json();
+
+            if (responseData.valid) {
+                window.location.href = 'basic.html';
+            } else {
+                alert('Invalid username or password. Please try again.');
+            }
+        } catch (error) {
+            // Handle fetch or network-related errors
+            console.error('Error:', error);
+            alert('An error occurred. Please try again later.');
+        }
+    });
+});
--- Popup/signup_popup.js
+++ Popup/signup_popup.js
@@ -3,3 +3,55 @@ const backButton = document.getElementById("backButton");
 backButton.addEventListener("click", () => {
   window.location.href = "popup.html";
 });
+
+// Signup API Implementation.
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form');
+  
+  form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      
+      // Get form data
+      const formData = new FormData(form);
+      const userData = {
+          name: formData.get('name'),
+          username: formData.get('username'),
+          email: formData.get('email'),
+          password: formData.get('password')
+      };
+      
+      try {
+          const response = await fetch('https://your-api-endpoint.com/validate', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  // Add any other necessary headers for authentication or security tokens
+              },
+              body: JSON.stringify(userData)
+          });
+
+          if (!response.ok) {
+              throw new Error('Network response was not ok.');
+          }
+
+          // Parse the API response
+          const responseData = await response.json();
+
+          // Check the validation status returned from the API
+          if (responseData.valid) {
+              // User details are valid
+              // Redirect to a success page or perform further actions
+              window.location.href = 'basic.html';
+          } else {
+              // Handle invalid user details
+              // Display error message or take appropriate action
+              alert('Invalid user details. Please check and try again.');
+          }
+      } catch (error) {
+          // Handle fetch or network-related errors
+          console.error('Error:', error);
+          alert('An error occurred. Please try again later.');
+      }
+  });
+});
