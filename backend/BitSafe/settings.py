240d52845dac1dfb70fb732d8548455fc93fb2da
Nithin <143109019+nithinchukka@users.noreply.github.com>
2024-01-08 17:54:36 +0530

    Update settings.py

--- backend/BitSafe/settings.py
+++ backend/BitSafe/settings.py
@@ -137,4 +137,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'chukkanithin@gmail.com'
-EMAIL_HOST_PASSWORD = 'vjex xhul osso syay'
+EMAIL_HOST_PASSWORD = ''
